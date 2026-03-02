d208fa98e2ee27e95ba468d30c558da515e7a188
Skydan Dmytro <skydan.dmytro@coralsoft.io>
2026-03-02 12:27:29 +0100

    feat: first pwa version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: false,
+  skipWaiting: true,
+  buildExcludes: [/app-build-manifest\.json$/],
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
